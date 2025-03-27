b7520cde72eebeb9da25b283e6c81de319fe9b7f
unknown <cmendoza26@horizon.csueastbay.edu>
2025-03-26 18:36:28 -0700

    added time

--- scraper.py
+++ scraper.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # query
 URL = 'https://www.google.com/finance/quote/NVDA:NASDAQ'
 html = requests.get(URL)
 soup = BeautifulSoup(html.content, 'html.parser')
+
 # find price div
 price = soup.select_one('div[data-last-price]')
 currentPrice = price.get('data-last-price')
-print(currentPrice)
+
+# display NVIDIA stock price
+print("Current price: " + currentPrice)
+print("Time: " + str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
